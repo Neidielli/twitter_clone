f2ef00bc2d032bd41d7e268d7b44b3687d3ac362
Neidielli <100215137+Neidielli@users.noreply.github.com>
2023-02-11 16:47:17 -0300

    Initial commit

---
+++ App/Models/Usuario.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use MF\Model\Model;
+
+class Usuario extends Model {
+
+    private $id;
+    private $nome;
+    private $email;
+    private $senha;
+
+    public function __get($atributo) {
+        return $this->$atributo;
+    }
+
+    public function __set($atributo, $valor) {
+        $this->$atributo = $valor;
+    }
+
+    // salvar user
+
+    // validadar cadastro
+
+    // recuperar user por e-mail
+}
+
+?>
