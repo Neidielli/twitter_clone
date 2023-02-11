07998c3fbb4a210c52f8be7404b0bea332473eb2
Neidielli <100215137+Neidielli@users.noreply.github.com>
2023-02-11 18:15:30 -0300

    Cadastro de usuários

--- App/Controllers/IndexController.php
+++ App/Controllers/IndexController.php
@@ -14,7 +14,42 @@ class IndexController extends Action {
 	}
 
 	public function inscreverse() {
+		$this->view->usuario = array( // coloca dados vazios no forms
+			'nome' => '',
+			'email' => '',
+			'senha' => '',
+		);
+		$this->view->erroCadastro = false;
+
 		$this->render('inscreverse');
+	}
+
+	public function registrar() {
+
+		// receber os dados do formulário
+		$usuario = Container::getModel('Usuario');
+
+		$usuario->__set('nome', $_POST['nome']);
+		$usuario->__set('email', $_POST['email']);
+		$usuario->__set('senha', $_POST['senha']);
+
+		if($usuario->validarCadastro() && count($usuario->getUsuarioPorEmail()) == 0) { // se cadastro for válido e n houver nenhum registro no bd
+			
+			$usuario->salvar();
+			$this->render('cadastro');
+
+		} else {
+
+			$this->view->usuario = array( // recarrega as info no forms
+				'nome' => $_POST['nome'],
+				'email' => $_POST['email'],
+				'senha' => $_POST['senha'],
+			);
+
+			$this->view->erroCadastro = true;
+
+			$this->render('inscreverse');
+		}
 	}
 
 }
--- App/Models/Usuario.php
+++ App/Models/Usuario.php
@@ -20,10 +20,43 @@ class Usuario extends Model {
     }
 
     // salvar user
+    public function salvar() {
+        $query = "insert into usuarios(nome, email, senha)values(:nome, :email, :senha)";
+        $stmt = $this->db->prepare($query);
+        $stmt->bindValue(':nome', $this->__get('nome'));
+        $stmt->bindValue(':email', $this->__get('email'));
+        $stmt->bindValue(':senha', $this->__get('senha')); // md5() -> hash de 32 caracteres
+        $stmt->execute();
+
+        return $this;
+    }
 
     // validadar cadastro
+    public function validarCadastro() {
+        $valido = true;
+
+        if(strlen($this->__get('nome')) < 3) {
+            $valido = false;
+        }
+        if(strlen($this->__get('email')) < 3) {
+            $valido = false;
+        }
+        if(strlen($this->__get('senha')) < 3) {
+            $valido = false;
+        }
+
+        return $valido;
+    }
 
     // recuperar user por e-mail
+    public function getUsuarioPorEmail() {
+        $query = "select nome, email from usuarios where email = :email";
+        $stmt = $this->db->prepare($query);
+        $stmt->bindValue(':email', $this->__get('email'));
+        $stmt->execute();
+
+        return $stmt->fetchAll(\PDO::FETCH_ASSOC);
+    }
 }
 
 ?>
