ca5cbffe1e2e213459642f809f113fb4bbfbd587
Neidielli <100215137+Neidielli@users.noreply.github.com>
2023-02-13 22:04:00 -0300

    Autenticação de usuário

---
+++ App/Controllers/AppController.php
@@ -0,0 +1,22 @@
+<?php 
+namespace App\Controllers;
+
+//os recursos do miniframework
+use MF\Controller\Action;
+use MF\Model\Container;
+
+class AppController extends Action {
+
+    public function timeline() {
+        session_start();
+
+        if($_SESSION['id'] != '' && $_SESSION['id'] != '') {
+            $this->render('timeline');
+        } else {
+            header('location: /?login=erro');
+        }
+    }
+}
+
+?>
+   
